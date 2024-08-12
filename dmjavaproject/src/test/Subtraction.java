0c075ace9be0d7dab3495cbfb26f9f0450d9b48d
Deepakm <deepakmautomation@gmail.com>
2024-08-12 12:29:30 +0530

    create java subtraction program

---
+++ dmjavaproject/src/test/Subtraction.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+
+		Subtraction sub = new Subtraction();
+		sub.subtract(10,1);
+	}
+
+	public int subtract(int a, int b) {
+
+		return a-b;
+	}
+
+}
